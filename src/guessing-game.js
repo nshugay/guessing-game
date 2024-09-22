6cd5b6d3741deeb3f37eada393e88f336c692c1b
nshugay <nshugay1@gmail.com>
2024-09-22 12:39:36 +0300

    feat: pass test

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,28 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+        this.min = 0;
+        this.max = 0;
+        this.mid = 0;
+    }
 
     setRange(min, max) {
-
+        this.min = min;
+        this.max = max;
+        this.mid = Math.ceil((this.min + this.max) / 2);
     }
 
     guess() {
-
+        return this.mid;
     }
 
     lower() {
-
+        this.max = this.mid;
+        this.mid = Math.ceil((this.min + this.max) / 2);
     }
 
     greater() {
-
+        this.min = this.mid;
+        this.mid = Math.ceil((this.min + this.max) / 2);
     }
 }
 
